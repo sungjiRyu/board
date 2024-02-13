65c3b078680ec432baf418ea819edf171ffb49e2
sungjiRyu <dhftlcm@gmail.com>
2024-02-14 00:53:14 +0900

    백엔드-프론트 공통 인터페이스 작성

---
+++ board-back/src/main/java/com/sjryu/boardback/dto/object/CommentListItem.java
@@ -0,0 +1,15 @@
+package com.sjryu.boardback.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentListItem {
+    private String nickname;
+    private String profileImage;
+    private String writerDatetime;
+    private String content;
+}
