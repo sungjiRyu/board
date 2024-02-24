8ae7196332a95c969df745f6e5e60f9f205cbe1e
sungjiRyu <dhftlcm@gmail.com>
2024-02-24 13:55:38 +0900

    특정 게시물 상세보기 컴포넌트 이벤트 처리

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,15 @@
+import { Board } from "types/enum/interface";
+
+const boardMock: Board = {
+
+    boardNumber : 1,
+    title : '오늘점심뭐먹지 맛있는거 먹고 싶은데',
+    content : '오늘점심뭐먹지 너무 고민이 되는데 추천좀',
+    boardImageList : ['https://image.edaily.co.kr/images/photo/files/NP/S/2023/05/PS23050400683.jpg', 'https://dimg.donga.com/wps/NEWS/IMAGE/2020/12/06/104306172.2.jpg'],
+    writeDatetime : '2024. 02. 24',
+    writerEmail : 'email@email.com',
+    writerNickname : '소드마스터최민식',
+    writerProfileImage : 'https://img.newspim.com/news/2020/12/31/2012310952406450.jpg'
+}
+
+export default boardMock;
